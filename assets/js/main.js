586b748972b63b3c150a69538e1aacb561df0c47
fanlore <fanlore@mail.ru>
2020-11-14 10:55:03 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,8 @@ var mySwiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-        clickable:true
-      },
+        clickable: true
+    },
 
     // Navigation arrows
     navigation: {
@@ -32,8 +32,8 @@ var mySwiperStories = new Swiper('.swiper-container-stories', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-        clickable:true
-      },
+        clickable: true
+    },
     slidesPerView: 3,
     breakpoints: {
         // when window width is >= 320px
@@ -110,31 +110,67 @@ buttons.forEach(function (button) {
 var open = document.querySelector('.open');
 var close = document.querySelector('.close');
 var mobNav = document.querySelector('.mobile-navigation');
-
+var page = document.querySelector('.page');
 open.addEventListener('click', function () {
     mobNav
         .classList
         .add('active');
+
 })
 close.addEventListener('click', function () {
     mobNav
         .classList
         .remove('active');
+
 })
-/*  плавный переход по якорям */ 
+/*  плавный переход по якорям */
 
 const anchors = document.querySelectorAll('a[href*="#"]');
 
-for(let anchor of anchors) {
-    anchor.addEventListener('click',function(e){
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const blockID = anchor.getAttribute('href');
-        document.querySelector(' ' + blockID).scrollIntoView({
-            behavior:"smooth",
-            block:"start"
-        });
+        document
+            .querySelector(' ' + blockID)
+            .scrollIntoView({behavior: "smooth", block: "start"});
     })
 }
-/* video photo gfallery*/ 
-require('fslightbox');
+/*
+window.addEventListener('scroll',function(){
+    let header = document.querySelector('.header');
+    if(window.pageYOffset > 400) {
+        header.classList.add('header-fixed');
+    }else {
+        header.classList.remove('header-fixed');
+    }
+});
+*/
+const scroll = function (e) {
+    const element = document.querySelector(".header");
+    let scrolltop = pageYOffset;
+    window.addEventListener("scroll", function () {
+        if (mobNav.classList.contains('active') === false) {
+            if (pageYOffset > scrolltop) {
+                element
+                    .classList
+                    .add("header-up");
+                element
+                    .classList
+                    .remove("header-down");
+            } else {
+                element
+                    .classList
+                    .add("header-down");
+                element
+                    .classList
+                    .remove("header-up");
+            }
+        }
+        scrolltop = pageYOffset;
+    });
+};
+scroll();
 
+/* video photo gfallery*/
+require('fslightbox');
